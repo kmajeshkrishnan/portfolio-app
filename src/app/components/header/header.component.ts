b40397bef440e9c5949bedb6eb920e923f737d40
humblefool <kmajeshkrishnan@gmail.com>
2025-04-13 20:17:36 +0200

    Changes for responsiveness

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,7 +11,8 @@ import { ThemeService } from '../../services/theme.service';
   imports: [CommonModule, RouterModule]
 })
 export class HeaderComponent {
-  isDarkTheme = true;
+  isDarkTheme = false;
+  isMobileMenuOpen = false;
 
   constructor(private themeService: ThemeService) {}
 
@@ -23,5 +24,17 @@ export class HeaderComponent {
 
   toggleTheme() {
     this.themeService.toggleTheme();
+    this.isDarkTheme = !this.isDarkTheme;
+    document.body.classList.toggle('dark-theme');
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    document.body.style.overflow = this.isMobileMenuOpen ? 'hidden' : '';
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+    document.body.style.overflow = '';
   }
 } 
